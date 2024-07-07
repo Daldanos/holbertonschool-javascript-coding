ba3aed1f6b6e094c7eaf4063dbc2ecf056b89832
German Silveira <adolfogsilveira@gmail.com>
2024-07-07 16:42:59 +0000

    changes

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieID = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieID}`;
+
+request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
+});
