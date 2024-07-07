9beea580d4757b2bdf3ad5d53520de03f1f0b8bf
German Silveira <adolfogsilveira@gmail.com>
2024-07-07 17:08:16 +0000

    Changes

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,13 +6,14 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    fs.writeFile(filePath, body, 'utf8', (err) => {
-      if (err) throw err;
-        console.log(`Webpage content saved to ${filePath}`);
-    });
-  } else {
-    console.log('Error fetching webpage content');
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
   }
+  fs.writeFile(filePath, body, 'utf8', function (err) {
+    if (err) {
+      console.error('Error writing to file:', err);
+    }
+  });
 });
