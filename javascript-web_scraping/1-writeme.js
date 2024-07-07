f0e7859176cbdb144976a07b5baced2682020d41
German Silveira <adolfogsilveira@gmail.com>
2024-07-07 16:33:20 +0000

    Changes

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,12 +2,11 @@
 
 const fs = require('fs');
 
-function writeToFile(filePath, content) {
-  fs.writeFile(filePath, content, 'utf-8', (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`Successfully wrote to ${filePath}`);
-    }
-  });
-}
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
