b6a6dba684340c22fc9a81e86a1a0d0f2ee44a63
German Silveira <adolfogsilveira@gmail.com>
2024-07-10 00:31:58 +0000

    changes

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 process.stdin.setEncoding('utf8');
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\r\n');
 
 process.stdin.on('data', (data) => {
   const input = data.toString().trim();
-  if (input === 'exit\n') {
-    process.stdout.write('This important software is now closing\n');
+  if (input === 'exit') {
+    process.stdout.write('This important software is now closing\r\n');
     process.exit();
   } else {
-    process.stdout.write(`Your name is: ${input}\n`);
+    process.stdout.write(`Your name is: ${input}\r\n`);
   }
 });
