f66a41e2060a3ceda863c74698541e472bfc3054
German Silveira <adolfogsilveira@gmail.com>
2024-07-10 00:24:18 +0000

    Changes

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write("Welcome to Holberton School, what is your name?\r\n");
 
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write("This important software is now closing\n");
+  process.stdout.write(`Your name is: ${name}\r\n`);
+  process.stdout.write("This important software is now closing\r\n");
   process.exit();
 });
